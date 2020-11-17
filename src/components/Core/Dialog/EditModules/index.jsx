cffeb86137241475df1cdc21448d5a4d8326a44a
Yurii Topchii <yurii.topchii11@gmail.com>
2020-11-17 11:55:51 +0200

    add checkbox modules

--- src/components/Core/Dialog/EditModules/index.jsx
+++ src/components/Core/Dialog/EditModules/index.jsx
@@ -13,13 +13,12 @@ export default function EditModules({
   open, handleClose, title, checkedItem
 }) {
   const dispatch = useDispatch();
-  const [state, setState] = React.useState({
+  const [modules, setModules] = React.useState({
     logbook: true,
-    events: true,
-    reports: true,
+    events: false,
+    reports: false,
     schedule: true,
     activity_log: true,
-
     create_places: true,
     create_jobs: true,
     create_groups: true,
@@ -29,17 +28,77 @@ export default function EditModules({
 
   const { t } = useTranslation();
 
-  const onChange = () => {
+  const handleChange = (event) => {
+    setModules({ ...modules, [event.target.name]: event.target.checked });
+  };
 
-  }
   return(
     <Dialog handleClose={handleClose} open={open} title={title}>
       <div className={style.addOrg}>
-        <div className={style.addOrg__inner}>
-          <Checkbox onChange={onChange} />
+        <div className={style.addOrg__inner2}>
+          <Checkbox 
+            onChange={handleChange} 
+            checked={modules.logbook} 
+            label={t('Logbook')}
+            name='logbook' 
+          />
+          <Checkbox 
+            onChange={handleChange} 
+            checked={modules.events} 
+            label={t('Events')}
+            name='events' 
+          />
+          <Checkbox 
+            onChange={handleChange} 
+            checked={modules.reports} 
+            label={t('Reports')}
+            name='reports' 
+          />
+          <Checkbox 
+            onChange={handleChange} 
+            checked={modules.reports} 
+            label={t('Schedule')}
+            name='schedule' 
+          />
+          <Checkbox 
+            onChange={handleChange} 
+            checked={modules.reports} 
+            label={t('Activity Log')}
+            name='activity_log' 
+          />
+           <div className={style.buttonBlock}></div>
         </div>
-        <div className={style.addOrg__inner}>
-          {checkedItem}
+        <div className={style.addOrg__inner2}>
+          <Checkbox 
+              onChange={handleChange} 
+              checked={modules.create_places} 
+              label={t('Can create Places')}
+              name='create_places' 
+            />
+            <Checkbox 
+              onChange={handleChange} 
+              checked={modules.create_jobs} 
+              label={t('Can create Jobs')}
+              name='create_jobs' 
+            />
+            <Checkbox 
+              onChange={handleChange} 
+              checked={modules.create_groups} 
+              label={t('Can create Groups')}
+              name='create_groups' 
+            />
+            <Checkbox 
+              onChange={handleChange} 
+              checked={modules.use_manager_mobile} 
+              label={t('Can use Manager Mobile View')}
+              name='use_manager_mobile' 
+            />
+            <Checkbox 
+              onChange={handleChange} 
+              checked={modules.use_approval_flow} 
+              label={t('Can use Approval Flow in Logbook')}
+              name='use_approval_flow' 
+            />
           <div className={style.buttonBlock}>
               <Button cancel size="big" onClick={handleClose}>{t('Cancel')}</Button>
               <Button 
