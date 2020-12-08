47333971425e737c12b3f9845c3a6c8eff60b84c
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-08 10:42:39 +0200

    add Account Sub-groups table data

--- src/components/Settings/Accounts/Grouping/SubgroupsBlock.jsx
+++ src/components/Settings/Accounts/Grouping/SubgroupsBlock.jsx
@@ -6,24 +6,29 @@ import Tooltip from '../../../Core/Tooltip';
 import Button from '../../../Core/Button/Button';
 import DataTable from '../../../Core/DataTableCustom/OLT';
 
-export default function SubgroupsBlock({ style }) {
-  const { t } = useTranslation();
+const columns = [
+  { label: 'Title', field: 'name', checked: true },
+  { label: 'Amount', field: 'users', checked: true },
+  { label: 'ID', field: 'id', checked: true },
+];
 
+export default function SubgroupsBlock({ style, selected: selectedGroup, subgroups = [] }) {
+  const { t } = useTranslation();
   return (
     <div className={style.categoryBlock}>
       <div className={style.labelBLock}>
         <Label text={t('Sub-groups')} htmlFor='' />
         <Tooltip title='Select Sub-group' />
         <div className={style.right}>
-          <Button onClick={() => ({ })} inverse inline size='small'>
+          <Button onClick={() => ({ })} inverse inline size='small' disabled={!selectedGroup}>
             {`+ ${t('add new sub-group')}`}
           </Button>
         </div>
       </div>
       <div>
         <DataTable
-          data={[]}
-          columns={[]}
+          data={subgroups ?? []}
+          columns={columns ?? []}
           columnsWidth={{}}
           onColumnsChange={() => {}}
           sortable
