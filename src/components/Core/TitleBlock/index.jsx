9d5470e38acf30ee98a018489d599868079f75f3
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-09 17:04:03 +0200

    modifying TitleBlock

--- src/components/Core/TitleBlock/index.jsx
+++ src/components/Core/TitleBlock/index.jsx
@@ -1,35 +1,67 @@
 import React from 'react';
 import Button from '../Button/Button';
 import styles from './Title.module.scss';
+import Tooltip from '../Tooltip';
 
 export default function TitleBlock({
   children,
   title,
   info,
+  infoReverse = false,
   TitleButtonNew,
-  handleButtonNew,
-  TitlebuttonImport,
-  handleButtonImport }) {
+  handleButtonNew = () => ({}),
+  TitleButtonImport,
+  handleButtonImport = () => ({}),
+  tooltip,
+}) {
   return (
     <div className={styles.titleBlock}>
       <div className={styles.leftBlock}>
         {children}
         <h4 className={styles.titleBlock__title}>{title}</h4>
         {
-          info &&
-          <div className={styles.infoBlock}>
-            {Object.keys(info).map(infoId => (
-              <span key={infoId} className={styles.infoBlock__inner}>
-                {infoId} {info[infoId]}
-              </span>
-            ))}
-          </div>
+          info
+          && (
+            <div className={styles.infoBlock}>
+              {Object.keys(info)
+                .map((infoId) => (
+                  <span key={infoId} className={styles.infoBlock__inner}>
+                    {
+                      infoReverse
+                        ? (
+                          <>
+                            {infoId}
+                            {' '}
+                            {info[infoId]}
+                          </>
+                        )
+                        : (
+                          <>
+                            {info[infoId]}
+                            {' '}
+                            {infoId}
+                          </>
+                        )
+                    }
+                  </span>
+                ))}
+            </div>
+          )
+        }
+        {
+          tooltip && <Tooltip title={tooltip} />
         }
       </div>
-      {
-        TitleButtonNew &&
-        <Button inverse onClick={() => handleButtonNew()}>{TitleButtonNew}</Button>
-      }
+      <div className={styles.buttonBlock}>
+        {
+          TitleButtonNew
+          && <Button inverse onClick={() => handleButtonNew()}>{TitleButtonNew}</Button>
+        }
+        {
+          TitleButtonImport
+          && <Button inverse onClick={() => handleButtonImport()}>{TitleButtonImport}</Button>
+        }
+      </div>
     </div>
-  )
+  );
 }
