9f5601ccf5f9acf36c76e049849c2c3e95a605b7
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-14 17:34:23 +0200

    email verification

---
+++ src/components/Helpers/emailValidation.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export,max-len
+export const validateEmail = (email) => /[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(email);
