32ac79b6cc89ad01d0458f3b1ad0fb9ffffcfa80
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-16 11:51:08 +0200

    creating new role, selecting role, added accessibility

--- src/components/Settings/Accounts/Roles/RolesBlock.jsx
+++ src/components/Settings/Accounts/Roles/RolesBlock.jsx
@@ -2,43 +2,62 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import classnames from 'classnames';
 import classes from './Roles.module.scss';
+import AddRolesIcon from '../../../Icons/AddRolesIcon';
+import RemoveRoleIcon from '../../../Icons/RemoveRoleIcon';
 
-function RolesBlock({ roles = [] }) {
+function RolesBlock({
+  roles = [],
+  activeRole,
+  setActiveRole,
+  createNewRole,
+}) {
   const { t } = useTranslation();
 
   return (
     <div className={classes.roles}>
 
-      <div className={classnames(classes.card, classes.default)}>
+      <div
+        className={classnames(classes.card, classes.default)}
+        onClick={() => ({})}
+        onKeyDown={() => ({ })}
+        role='option'
+        aria-selected='true'
+        aria-label='create new role'
+        tabIndex={0}
+      >
         <p className={classes.card_title}>New role</p>
         <small>{t('Create a new role')}</small>
+        <span
+          className={classes.card_icon}
+        >
+          <AddRolesIcon aria-hidden />
+        </span>
       </div>
-
-      <div className={classes.card}>
-        <p className={classes.card_title}>New role</p>
-        <small>{t('Create a new role')}</small>
-      </div>
-      {' '}
-      <div className={classes.card}>
-        <p className={classes.card_title}>New role</p>
-        <small>{t('Create a new role')}</small>
-      </div>
-      {' '}
-      <div className={classes.card}>
-        <p className={classes.card_title}>New role</p>
-        <small>{t('Create a new role')}</small>
-      </div>
-      {' '}
-      <div className={classes.card}>
-        <p className={classes.card_title}>New role</p>
-        <small>{t('Create a new role')}</small>
-      </div>
-      {' '}
-      <div className={classes.card}>
-        <p className={classes.card_title}>New role</p>
-        <small>{t('Create a new role')}</small>
-      </div>
-
+      {
+        roles.map((role) => (
+          <>
+            <div
+              className={classnames(classes.card, role.id === activeRole.id ? classes.active : '')}
+              onClick={() => setActiveRole(role)}
+              onKeyDown={() => setActiveRole(role)}
+              key={role.id + role.name}
+              role='option'
+              aria-label='user role'
+              aria-selected='true'
+              tabIndex={0}
+            >
+              <p className={classes.card_title}>{role.name}</p>
+              <small>{`${role.account_user_roles?.length} ${t('users have this role')}`}</small>
+              <button
+                className={classes.card_icon}
+                aria-label='remove role button'
+              >
+                <RemoveRoleIcon aria-hidden />
+              </button>
+            </div>
+          </>
+        ))
+      }
     </div>
   );
 }
