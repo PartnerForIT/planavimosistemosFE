401d382f22647b73c5467dec432606301c2ea985
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-17 17:11:28 +0200

    add scroll bars

--- src/components/Settings/Accounts/Roles/RoleDetails/Content.jsx
+++ src/components/Settings/Accounts/Roles/RoleDetails/Content.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Scrollbar from 'react-scrollbars-custom';
+import classNames from 'classnames';
 import classes from '../Roles.module.scss';
 import Tooltip from '../../../../Core/Tooltip';
+import styles from '../../../../Core/DataTableCustom/DTM.module.scss';
 
 function Content({ children, title = '', tooltip = '' }) {
   const { t } = useTranslation();
@@ -14,7 +17,37 @@ function Content({ children, title = '', tooltip = '' }) {
           <span><Tooltip title={t(tooltip)} /></span>
         </p>
         <div className={classes.content}>
-          {children}
+          <Scrollbar
+            className='scrollableContentClasses'
+            style={{ height: 447 }}
+            removeTracksWhenNotUsed
+            trackXProps={{
+              renderer: (props) => {
+                const { elementRef, ...restProps } = props;
+                return (
+                  <span
+                    {...restProps}
+                    ref={elementRef}
+                    className={classNames(styles.scrollbarTrackX, { trackX: true })}
+                  />
+                );
+              },
+            }}
+            trackYProps={{
+              renderer: (props) => {
+                const { elementRef, ...restProps } = props;
+                return (
+                  <span
+                    {...restProps}
+                    ref={elementRef}
+                    className={classNames(styles.scrollbarTrackY, { trackY: true })}
+                  />
+                );
+              },
+            }}
+          >
+            {children}
+          </Scrollbar>
         </div>
       </div>
     </div>
