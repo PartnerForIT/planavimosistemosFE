1e1be85ed34bd0b3882e20911936ea60ab6cd083
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-18 12:57:01 +0200

    working on OrganisationAccess options

--- src/components/Settings/Accounts/Roles/RoleDetails/OrganisationAccess.jsx
+++ src/components/Settings/Accounts/Roles/RoleDetails/OrganisationAccess.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Content from './Content';
 
-function OrganisationAccess({ availableDetails }) {
+function OrganisationAccess({ availableDetails, categoriesNames, roleAccess }) {
+  const available = useMemo(() => availableDetails.map((item) => ({ [item.name]: item.action })), [availableDetails]);
+
   return (
     <Content tooltip='Tooltip' title='Organisation access'>
-      content
+
+      {/* Use Managers Mobile View */}
+      {/* ???? */}
+
+      {/* Can edit General Settings */}
+      {/* pto ~> edit_settings */}
+
+      {/* Can create Groups */}
+      {/* groups ~> create */}
+
+      {/* Can create Roles */}
+      {/* roles ~> create */}
+
+      {/* Can delete entry data */}
+      {/* data ~> delete */}
+
+      {/* Can create Categories */}
+      {/* categories ~> create */}
+
+      {/* Can create New accounts */}
+      {/* accounts ~> create */}
+
+      {/* Can see & edit Accounts List */}
+      {/* ??? */}
+
+      {/* Can delete Accounts list */}
+      {/* ??? */}
+
+      {/* Can edit Logbook settings */}
+      {/* ??? */}
+
+      {/* Can create Events */}
+      {/* events ~> create */}
+
+      {/* Can see Activity Log */}
+      {/* activity_log ~> view */}
+
+      <pre>
+        {/* {JSON.stringify(roleAccess, null, 2)} */}
+        {JSON.stringify(available, null, 2)}
+      </pre>
     </Content>
   );
 }
