bf384d1cfbe728e5a35faa2a7330fe318d14e629
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-18 12:33:57 +0200

    memoization

--- src/components/Settings/Accounts/Roles/RoleDetails/AccessModule.jsx
+++ src/components/Settings/Accounts/Roles/RoleDetails/AccessModule.jsx
@@ -2,40 +2,38 @@ import React from 'react';
 import Content from './Content';
 import OptionsCheckBoxGroup from './OptionsCheckboxGroup';
 
-function AccessModule({
+const AccessModule = React.memo(({
   availableDetails = [],
   roleAccess = {},
   categoriesNames = {},
-}) {
-  return (
+}) => (
 
-    <Content title='Access by module' tooltip='Tooltip'>
+  <Content title='Access by module' tooltip='Tooltip'>
 
-      {/* Logbook */}
-      <OptionsCheckBoxGroup
-        name='logbook'
-        availableDetails={availableDetails}
-        categoriesNames={categoriesNames}
-        roleAccess={roleAccess}
-      />
+    {/* Logbook */}
+    <OptionsCheckBoxGroup
+      name='logbook'
+      availableDetails={availableDetails}
+      categoriesNames={categoriesNames}
+      roleAccess={roleAccess}
+    />
 
-      {/* Reports */}
-      <OptionsCheckBoxGroup
-        name='reports'
-        availableDetails={availableDetails}
-        categoriesNames={categoriesNames}
-        roleAccess={roleAccess}
-      />
+    {/* Reports */}
+    <OptionsCheckBoxGroup
+      name='reports'
+      availableDetails={availableDetails}
+      categoriesNames={categoriesNames}
+      roleAccess={roleAccess}
+    />
 
-      {/* Events */}
-      <OptionsCheckBoxGroup
-        name='events'
-        availableDetails={availableDetails}
-        categoriesNames={categoriesNames}
-        roleAccess={roleAccess}
-      />
-    </Content>
-  );
-}
+    {/* Events */}
+    <OptionsCheckBoxGroup
+      name='events'
+      availableDetails={availableDetails}
+      categoriesNames={categoriesNames}
+      roleAccess={roleAccess}
+    />
+  </Content>
+));
 
 export default AccessModule;
