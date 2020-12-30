d23f31d89b822db55c2b0c9fe313b73e15ec5f31
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-30 17:50:21 +0200

    add access to menu from keyboard - accessibility

--- src/components/Core/Header/Avatar.jsx
+++ src/components/Core/Header/Avatar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import styles from './header.module.scss';
+import { onKeyDown } from '../../Helpers';
 
 export default function AvatarComponent({
   setAnchorEl = () => ({}), setMenuOpen = () => ({}),
 }) {
   const user = JSON.parse(localStorage.getItem('user'));
 
+  const clickHandler = (e) => {
+    setAnchorEl(e.currentTarget);
+    setMenuOpen(true);
+  };
+
   return (
     <div
+      tabIndex={0}
       className={styles.userBlock}
-      onClick={(e) => {
-        setAnchorEl(e.currentTarget);
-        setMenuOpen(true);
-      }}
-      onKeyDown={() => ({})}
+      onClick={clickHandler}
+      role='menu'
+      onKeyDown={(event) => onKeyDown(event, () => clickHandler(event))}
     >
       <Avatar>{user?.employee?.photo}</Avatar>
       <span className={styles.userBlock__name}>
