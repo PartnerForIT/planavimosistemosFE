70ce8b9aeb02a9eeadb31f390eeb68103f517724
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-30 14:01:15 +0200

    add icons for Menu

---
+++ src/components/Icons/GlobeIcon.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+import SvgIcon from '@material-ui/core/SvgIcon';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '1em',
+    height: '1em',
+  },
+}));
+
+export default function GlobeIcon() {
+  const classes = useStyles();
+  return (
+    <SvgIcon viewBox='0 0 15.4 15.4' className={classes.root}>
+      <g id='globe' transform='translate(-1.098 -1.098)'>
+        <circle id='Эллипс_153' data-name='Эллипс 153' cx='7' cy='7' r='7' transform='translate(1.798 1.798)' fill='none' stroke='#808f94' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.4' />
+        <line id='Линия_640' data-name='Линия 640' x2='14' transform='translate(1.798 8.798)' fill='none' stroke='#808f94' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.4' />
+        <path id='Контур_13760' data-name='Контур 13760' d='M10.719,2a10.4,10.4,0,0,1,2.719,6.8,10.4,10.4,0,0,1-2.719,6.8A10.4,10.4,0,0,1,8,8.8,10.4,10.4,0,0,1,10.719,2Z' transform='translate(-1.921)' fill='none' stroke='#808f94' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.4' />
+      </g>
+    </SvgIcon>
+  );
+}
---
+++ src/components/Icons/LockIcon.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable max-len */
+import SvgIcon from '@material-ui/core/SvgIcon';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '1em',
+    height: '1em',
+  },
+}));
+
+export default function LockIcon() {
+  const classes = useStyles();
+  return (
+    <SvgIcon viewBox='0 0 13.4 15.395' className={classes.root}>
+      <g id='lock' transform='translate(-2.596 -1.3)'>
+        <rect id='Прямоугольник_71848' data-name='Прямоугольник 71848' width='12' height='8' rx='2' transform='translate(3.296 7.995)' fill='none' stroke='#808f94' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.4' />
+        <path id='Контур_13756' data-name='Контур 13756' d='M7,8V5.331A3.417,3.417,0,0,1,10.5,2,3.417,3.417,0,0,1,14,5.331V8' transform='translate(-1.202)' fill='none' stroke='#808f94' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.4' />
+      </g>
+    </SvgIcon>
+  );
+}
