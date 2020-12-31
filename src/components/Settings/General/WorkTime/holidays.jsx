b698477b3c9447dfc55c0cc93a9fdcb3027b013e
Yurii Topchii <yurii.topchii11@gmail.com>
2020-12-31 11:23:28 +0200

    added Work Time Holiday's year selector

--- src/components/Settings/General/WorkTime/holidays.jsx
+++ src/components/Settings/General/WorkTime/holidays.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import * as _ from 'lodash';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import moment from 'moment';
 import Label from '../../../Core/InputLabel';
 import Tooltip from '../../../Core/Tooltip';
 import Button from '../../../Core/Button/Button';
 import AddHolidays from '../../../Core/Dialog/AddHoliday';
 import DeleteIcon from '../../../Icons/DeleteIcon';
 import { addHoliday, deleteHoliday } from '../../../../store/settings/actions';
-import { useDispatch } from 'react-redux';
-import moment from 'moment';
+import { onKeyDown } from '../../../Helpers';
+import ArrowLeftButton from '../../../Icons/ArrowLeftButton';
+import ArrowRightButton from '../../../Icons/ArrowRightButton';
 
-export default function Holidays({ styles, holidays, companyId, companyHolidys }) {
+export default function Holidays({
+  styles, holidays, companyId, companyHolidays, setYear = () => ({}), year = '',
+}) {
   const { t } = useTranslation();
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
 
   const [name, setName] = useState('');
@@ -23,30 +28,68 @@ export default function Holidays({ styles, holidays, companyId, companyHolidys }
   };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
+
   const saveAddHoliday = () => {
     const data = {
       name,
       company_work_time_id: companyId,
       date: moment(selectedDate).format('YYYY-MM-DD'),
-    }
-    dispatch(addHoliday(data, companyId))
-    setOpen(false)
-  }
+    };
+    dispatch(addHoliday(data, companyId));
+    setOpen(false);
+  };
 
   const delHoliday = (id) => {
-    dispatch(deleteHoliday(id, companyId))
-  }
+    dispatch(deleteHoliday(id, companyId));
+  };
+
+  const yearChangeHandler = (opt) => {
+    const currentYear = new Date().getFullYear();
+
+    if (opt > 0) {
+      setYear((prevState) => (prevState < currentYear + 2 ? prevState + opt : prevState));
+    }
+    if (opt < 0) {
+      setYear((prevState) => (prevState > currentYear - 2 ? prevState + opt : prevState));
+    }
+  };
 
   return (
     <div className={styles.holidays}>
       <div className={styles.labelButtonBlock}>
         <div className={styles.labelBlock}>
           <Label text={t('Holidays')} />
-          <Tooltip title={'Add new Holidays'} />
+          <Tooltip title='Add new Holidays' />
         </div>
-        <Button inverse onClick={() => setOpen(true)} >
+
+        {/* year */}
+        <div className={styles.year}>
+          {/* left */}
+          <button
+            aria-label='year decreasing'
+            onClick={() => yearChangeHandler(-1)}
+          >
+            <ArrowLeftButton
+              aria-hidden
+              className={styles.icon}
+            />
+          </button>
+          <input value={year} disabled />
+          <button
+            aria-label='year increasing'
+            onClick={() => yearChangeHandler(1)}
+          >
+            <ArrowRightButton
+              aria-hidden
+              className={styles.icon}
+            />
+          </button>
+        </div>
+
+        {/* add new */}
+        <Button inverse onClick={() => setOpen(true)}>
           {t('+ add new')}
         </Button>
         <AddHolidays
@@ -60,23 +103,30 @@ export default function Holidays({ styles, holidays, companyId, companyHolidys }
           handleDateChange={handleDateChange}
         />
       </div>
-      {_.map(companyHolidys, (item) =>
+      {_.map(companyHolidays, (item) => (
         <div key={item.id + item.name} className={styles.holidays__block}>
           <div className={styles.holidays__innerBlock}>
             <div className={styles.holidays_data}>{item.date}</div>
             <div className={styles.holidays_name}>{item.name}</div>
           </div>
-          <span onClick={() => delHoliday(item.id)} className={styles.deleteHoliday}><DeleteIcon fill={"#fd0d1b"} viewBox={'0 0 32 14'} /></span>
+          <button
+            onClick={() => delHoliday(item.id)}
+            onKeyDown={(event) => onKeyDown(event, () => delHoliday(item.id))}
+            className={styles.deleteHoliday}
+            aria-label='delete holiday'
+          >
+            <DeleteIcon fill='#fd0d1b' viewBox='0 0 32 14' aria-hidden />
+          </button>
         </div>
-      )}
-      {_.map(holidays, (item) =>
+      ))}
+      {_.map(holidays, (item) => (
         <div key={item.name} className={styles.holidays__block}>
           <div className={styles.holidays__innerBlock}>
             <div className={styles.holidays_data}>{item.date}</div>
             <div className={styles.holidays_name}>{item.name}</div>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
--- src/components/Settings/General/WorkTime/index.jsx
+++ src/components/Settings/General/WorkTime/index.jsx
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
-import MaynLayout from '../../../Core/MainLayout';
+import Snackbar from '@material-ui/core/Snackbar';
+import MainLayout from '../../../Core/MainLayout';
 import PageLayout from '../../../Core/PageLayout';
 import TitleBlock from '../../../Core/TitleBlock';
 import Dashboard from '../../../Core/Dashboard';
 import {
   isLoadingSelector, isShowSnackbar,
-  snackbarType, snackbarText, settingWorkTime
+  snackbarType, snackbarText, settingWorkTime,
 } from '../../../../store/settings/selectors';
 import { getSettingWorkTime, patchWorkTime } from '../../../../store/settings/actions';
 import WorkTimeIcon from '../../../Icons/WorkTime';
 import Progress from '../../../Core/Progress';
-import Snackbar from '@material-ui/core/Snackbar';
 import Holidays from './holidays';
 import StartWeek from './StartWeek';
 import WorkingTime from './WorkingTime';
@@ -23,17 +22,16 @@ import styles from './workTime.module.scss';
 const useStyles = makeStyles(() => ({
   error: {
     background: '#de4343',
-    color: "#fff",
+    color: '#fff',
   },
   success: {
     background: '#3bc39e',
-    color: "#fff",
-  }
+    color: '#fff',
+  },
 }));
 
 export default function WorkTime() {
   const params = useParams();
-  const { t } = useTranslation();
   const dispatch = useDispatch();
   const classes = useStyles();
 
@@ -43,6 +41,7 @@ export default function WorkTime() {
   });
 
   const [startTime, setStartTime] = useState({});
+  const [year, setYear] = useState(new Date().getFullYear());
 
   const [days, setDays] = useState({
     monday: false,
@@ -52,67 +51,65 @@ export default function WorkTime() {
     friday: false,
     saturday: false,
     sunday: false,
-  })
+  });
 
   useEffect(() => {
     if (params.id) {
-      dispatch(getSettingWorkTime(params.id))
+      dispatch(getSettingWorkTime(params.id));
     }
-  }, []);
+  }, [dispatch, params.id]);
 
-  const isLoadind = useSelector(isLoadingSelector);
+  const isLoading = useSelector(isLoadingSelector);
   const isSnackbar = useSelector(isShowSnackbar);
   const typeSnackbar = useSelector(snackbarType);
   const textSnackbar = useSelector(snackbarText);
   const workTime = useSelector(settingWorkTime);
 
-  const filterWorksday = (idDay) => {
-    const arr = workTime.work_time.work_days.days.days.filter(item => item.day === idDay);
-    return arr;
-  }
+  const filterWorksDay = useCallback((idDay) => workTime.work_time?.work_days
+    ?.days?.days.filter((item) => item.day === idDay) ?? [],
+  [workTime]);
 
   useEffect(() => {
     setInputValues({
-      ...inputValues,
-      week_start: workTime.work_time.week_start || '1',
-      week_start_time: workTime.work_time.week_start_time || '08:00',
+
+      week_start: workTime.work_time?.week_start ?? '1',
+      week_start_time: workTime.work_time?.week_start_time ?? '08:00',
     });
     if (Object.keys(workTime.work_time).length > 0) {
       setDays({
-        monday: filterWorksday(1).length > 0,
-        tuesday: filterWorksday(2).length > 0,
-        wednesday: filterWorksday(3).length > 0,
-        thursday: filterWorksday(4).length > 0,
-        friday: filterWorksday(5).length > 0,
-        saturday: filterWorksday(6).length > 0,
-        sunday: filterWorksday(7).length > 0,
-      })
+        monday: filterWorksDay(1).length > 0,
+        tuesday: filterWorksDay(2).length > 0,
+        wednesday: filterWorksDay(3).length > 0,
+        thursday: filterWorksDay(4).length > 0,
+        friday: filterWorksDay(5).length > 0,
+        saturday: filterWorksDay(6).length > 0,
+        sunday: filterWorksDay(7).length > 0,
+      });
       setStartTime({
-        start1: (filterWorksday(1).length > 0 && filterWorksday(1)[0].start) ? filterWorksday(1)[0].start : '08:00',
-        finish1: (filterWorksday(1).length > 0 && filterWorksday(1)[0].finish) ? filterWorksday(1)[0].finish : '17:00',
-        start2: (filterWorksday(2).length > 0 && filterWorksday(2)[0].start) ? filterWorksday(2)[0].start : '08:00',
-        finish2: filterWorksday(2).length > 0 ? filterWorksday(2)[0].finish : '17:00',
-        start3: filterWorksday(3).length > 0 ? filterWorksday(3)[0].start : '10:00',
-        finish3: filterWorksday(3).length > 0 ? filterWorksday(3)[0].finish : '17:00',
-        start4: filterWorksday(4).length > 0 ? filterWorksday(4)[0].start : '08:00',
-        finish4: filterWorksday(4).length > 0 ? filterWorksday(4)[0].finish : '17:00',
-        start5: filterWorksday(5).length > 0 ? filterWorksday(5)[0].start : '08:00',
-        finish5: filterWorksday(5).length > 0 ? filterWorksday(5)[0].finish : '17:00',
-        start6: filterWorksday(6).length > 0 ? filterWorksday(6)[0].start : '08:00',
-        finish6: filterWorksday(6).length > 0 ? filterWorksday(6)[0].finish : '17:00',
-        start7: filterWorksday(7).length > 0 ? filterWorksday(7)[0].start : '08:00',
-        finish7: filterWorksday(7).length > 0 ? filterWorksday(7)[0].finish : '08:00',
-      })
+        start1: (filterWorksDay(1).length > 0 && filterWorksDay(1)[0].start) ? filterWorksDay(1)[0].start : '08:00',
+        finish1: (filterWorksDay(1).length > 0 && filterWorksDay(1)[0].finish) ? filterWorksDay(1)[0].finish : '17:00',
+        start2: (filterWorksDay(2).length > 0 && filterWorksDay(2)[0].start) ? filterWorksDay(2)[0].start : '08:00',
+        finish2: filterWorksDay(2).length > 0 ? filterWorksDay(2)[0].finish : '17:00',
+        start3: filterWorksDay(3).length > 0 ? filterWorksDay(3)[0].start : '08:00',
+        finish3: filterWorksDay(3).length > 0 ? filterWorksDay(3)[0].finish : '17:00',
+        start4: filterWorksDay(4).length > 0 ? filterWorksDay(4)[0].start : '08:00',
+        finish4: filterWorksDay(4).length > 0 ? filterWorksDay(4)[0].finish : '17:00',
+        start5: filterWorksDay(5).length > 0 ? filterWorksDay(5)[0].start : '08:00',
+        finish5: filterWorksDay(5).length > 0 ? filterWorksDay(5)[0].finish : '17:00',
+        start6: filterWorksDay(6).length > 0 ? filterWorksDay(6)[0].start : '08:00',
+        finish6: filterWorksDay(6).length > 0 ? filterWorksDay(6)[0].finish : '17:00',
+        start7: filterWorksDay(7).length > 0 ? filterWorksDay(7)[0].start : '08:00',
+        finish7: filterWorksDay(7).length > 0 ? filterWorksDay(7)[0].finish : '08:00',
+      });
     }
+  }, [filterWorksDay, workTime.work_time]);
 
-  }, [workTime])
-
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setInputValues({ ...inputValues, [name]: value });
   };
 
-  const handleChangeStartTime = event => {
+  const handleChangeStartTime = (event) => {
     const { name, value } = event.target;
     setStartTime({ ...startTime, [name]: value });
   };
@@ -123,70 +120,78 @@ export default function WorkTime() {
 
   const saveTime = () => {
     const daysData = [];
-    Object.values(days).map((item, index) => {
+    Object.values(days).forEach((item, index) => {
       if (item) {
-        daysData.push({ 'day': index + 1, 'start': startTime[`start${index + 1}`], 'finish': startTime[`finish${index + 1}`] })
+        daysData.push({
+          day: index + 1,
+          start: startTime[`start${index + 1}`],
+          finish: startTime[`finish${index + 1}`],
+        });
       }
-    })
+    });
     const data = {
       week_start: inputValues.week_start,
       week_start_time: inputValues.week_start_time,
       work_days: {
-        days: daysData
-      }
+        days: daysData,
+      },
     };
-    dispatch(patchWorkTime(data, params.id))
-  }
+    dispatch(patchWorkTime(data, params.id));
+  };
 
   return (
-    <MaynLayout>
+    <MainLayout>
       <Dashboard>
         <TitleBlock
-          title={"Work Time"}
+          title='Work Time'
         >
           <WorkTimeIcon />
         </TitleBlock>
         <PageLayout>
           {
-            isLoadind ? <Progress /> :
-              <>
-                <StartWeek
-                  styles={styles}
-                  days={workTime.days}
-                  workTime={workTime.work_time}
-                  handleInputChange={handleInputChange}
-                  inputValues={inputValues}
-                />
-                <WorkingTime
-                  styles={styles}
-                  days={days}
-                  handleChangeDays={handleChangeDays}
-                  startTime={startTime}
-                  handleChangeStartTime={handleChangeStartTime}
-                  saveTime={saveTime}
-                />
-                <Holidays
-                  styles={styles}
-                  holidays={workTime.national_holidays}
-                  companyHolidys={workTime.work_time.holidays}
-                  companyId={params.id}
-                />
-              </>
+            isLoading ? <Progress />
+              : (
+                <>
+                  <StartWeek
+                    styles={styles}
+                    days={workTime.days}
+                    workTime={workTime.work_time}
+                    handleInputChange={handleInputChange}
+                    inputValues={inputValues}
+                  />
+                  <WorkingTime
+                    styles={styles}
+                    days={days}
+                    handleChangeDays={handleChangeDays}
+                    startTime={startTime}
+                    handleChangeStartTime={handleChangeStartTime}
+                    saveTime={saveTime}
+                  />
+                  <Holidays
+                    styles={styles}
+                    holidays={workTime.national_holidays}
+                    companyHolidays={workTime.work_time.holidays}
+                    companyId={params.id}
+                    year={year}
+                    setYear={setYear}
+                  />
+                </>
+              )
           }
           <Snackbar
             anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
             ContentProps={{
               classes: {
-                root: typeSnackbar === 'error' ? classes.error : classes.success
-              }
+                root: typeSnackbar === 'error' ? classes.error : classes.success,
+              },
             }}
-            severity="error"
+            severity='error'
             open={isSnackbar}
             message={textSnackbar}
-            key={"rigth"}
+            key='right'
           />
         </PageLayout>
       </Dashboard>
-    </MaynLayout>
-  )
+    </MainLayout>
+  );
 }
