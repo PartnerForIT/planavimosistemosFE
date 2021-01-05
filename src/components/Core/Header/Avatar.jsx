d7c0f90a1108ae8befe4dc5a6cbc9512a96f837a
Yurii Topchii <yurii.topchii11@gmail.com>
2021-01-05 11:17:59 +0200

    fix avatar styles

--- src/components/Core/Header/Avatar.jsx
+++ src/components/Core/Header/Avatar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import styles from './header.module.scss';
 import { onKeyDown } from '../../Helpers';
@@ -13,6 +13,11 @@ export default function AvatarComponent({
     setMenuOpen(true);
   };
 
+  const shortName = useMemo(
+    () => user?.employee?.name?.slice(0, 1) + user?.employee?.surname?.slice(0, 1),
+    [user.employee.name, user.employee.surname],
+  );
+
   return (
     <div
       tabIndex={0}
@@ -22,10 +27,11 @@ export default function AvatarComponent({
       aria-haspopup='menu'
       onKeyDown={(event) => onKeyDown(event, () => clickHandler(event))}
     >
-      <Avatar aria-hidden>{user?.employee?.photo}</Avatar>
+      <Avatar aria-hidden src={user?.employee?.photo}>
+        {shortName}
+      </Avatar>
       <span className={styles.userBlock__name} aria-label='user shortened name'>
-        {user?.employee?.name?.slice(0, 1)}
-        {user?.employee?.surname?.slice(0, 1)}
+        {shortName}
       </span>
     </div>
   );
