1cd692c99125c56fdef29aabc65c15a20dee429d
Motsnyy Yuriy <ec5ta5ik@gmail.com>
2022-07-01 17:15:39 +0300

    fix working hours

--- src/components/Logbook/Logbook.jsx
+++ src/components/Logbook/Logbook.jsx
@@ -76,7 +76,7 @@ const columns = [
   { label: 'Start', field: 'start', checked: true },
   { label: 'End', field: 'end', checked: true },
   { label: 'Duration, h', field: 'duration', checked: true },
-  { label: 'Working hours', field: 'working_hours', checked: true },
+  { label: 'Working hours, h', field: 'working_hours', checked: true },
   { label: 'Night time, h', field: 'night_duration', checked: true },
   { label: <TextWithSign label='Cost' />, field: 'cost', checked: true },
   { label: <TextWithSign label='Earnings' />, field: 'charge', checked: true },
