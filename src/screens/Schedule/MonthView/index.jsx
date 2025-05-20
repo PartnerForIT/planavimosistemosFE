87679f1aa1839811a58cfcdb81f5530bd2723117
JulianBeletskyy <35832978+JulianBeletskyy@users.noreply.github.com>
2025-05-20 14:06:05 +0300

    add safari fix

--- src/screens/Schedule/MonthView/index.jsx
+++ src/screens/Schedule/MonthView/index.jsx
@@ -18,6 +18,25 @@ import RowContent from './RowContent';
 import Cell from './Cell';
 import classes from './MonthView.module.scss';
 
+window.requestIdleCallback = window.requestIdleCallback ||
+  function (cb) {
+    return setTimeout(() => {
+      const start = Date.now();
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        }
+      });
+    }, 1);
+  };
+
+window.cancelIdleCallback = 
+  window.cancelIdleCallback || 
+  function (id) {
+    clearTimeout(id);
+  };
+
 const trackYProps = {
   renderer: ({ elementRef, ...props }) => (
     <span
