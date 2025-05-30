64e00eae4841650ebec9ecaf42e129fb1d9183a3
JulianBeletskyy <35832978+JulianBeletskyy@users.noreply.github.com>
2025-05-30 17:54:47 +0300

    fix tooltip

--- src/screens/Schedule/MonthView/RowContent/index.jsx
+++ src/screens/Schedule/MonthView/RowContent/index.jsx
@@ -38,7 +38,7 @@ const RowContent = ({
 
   useEffect(() => {
     ReactTooltip.rebuild();
-  }, []);
+  });
 
   const newFoundItem = (day) => {
     const ev = events.find((item) => resourceId === item.resourceId && (item.day ? item.day*1 : item.day_number*1) === day*1);
