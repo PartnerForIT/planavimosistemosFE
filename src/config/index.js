891a84fa869b1ca374c1c4168a00d2a015d7ec1b
JulianBeletskyy <35832978+JulianBeletskyy@users.noreply.github.com>
2025-06-27 10:31:07 +0300

    change api

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 export default {
   api: {
-    url: process.env.REACT_APP_BACKEND_URL || 'https://app.grownu.com/api',
+    url: 'https://app.grownu.com/api',
   },
   google: {
     key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || 'AIzaSyDK6gV-4vYoIcst1DlL2cmeDhslD6L0vj4',
