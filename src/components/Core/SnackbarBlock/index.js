40cd5faf4fcb6a978984c64acaee3dd194117ea3
Motsnyy Yuriy <ec5ta5ik@gmail.com>
2025-10-17 08:41:51 +0300

    activity fixes

--- src/components/Core/SnackbarBlock/index.js
+++ src/components/Core/SnackbarBlock/index.js
@@ -5,10 +5,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 
 import {
-  isShowSnackbar,
-  snackbarText,
-  snackbarType,
+  isShowSnackbar as isShowOrgSnackbar,
+  snackbarText as orgSnackbarText,
+  snackbarType as orgSnackbarType,
 } from '../../../store/organizationList/selectors';
+
+import {
+  isShowSnackbar as isShowSettingsSnackbar,
+  snackbarText as settingsSnackbarText,
+  snackbarType as settingsSnackbarType,
+} from '../../../store/settings/selectors';
 
 const useStyles = makeStyles(() => ({
   error: {
@@ -25,9 +31,20 @@ export default () => {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  const isSnackbar = useSelector(isShowSnackbar);
-  const typeSnackbar = useSelector(snackbarType);
-  const textSnackbar = t(useSelector(snackbarText));
+  // Read from organizationList state
+  const isOrgSnackbar = useSelector(isShowOrgSnackbar);
+  const orgType = useSelector(orgSnackbarType);
+  const orgText = useSelector(orgSnackbarText);
+
+  // Read from settings state
+  const isSettingsSnackbar = useSelector(isShowSettingsSnackbar);
+  const settingsType = useSelector(settingsSnackbarType);
+  const settingsText = useSelector(settingsSnackbarText);
+
+  // Determine which snackbar to show (settings takes priority if both are active)
+  const isSnackbar = isSettingsSnackbar || isOrgSnackbar;
+  const typeSnackbar = isSettingsSnackbar ? settingsType : orgType;
+  const textSnackbar = t(isSettingsSnackbar ? settingsText : orgText);
 
   return (
     <Snackbar
